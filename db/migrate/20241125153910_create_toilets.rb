aec7840eeed599e0624c06f06ebe206c62c28c3c
Sidney Barth <sidneybarth91@gmail.com>
2024-11-26 11:58:53 +0100

    changed the database

--- db/migrate/20241125153910_create_toilets.rb
+++ db/migrate/20241125153910_create_toilets.rb
@@ -2,8 +2,9 @@ class CreateToilets < ActiveRecord::Migration[7.1]
   def change
     create_table :toilets do |t|
       t.string :location
-      t.string :category
-      t.boolean :avaliable
+      t.boolean :available
+      t.float :longitude
+      t.float :latitude
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
