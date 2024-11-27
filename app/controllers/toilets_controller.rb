04ee7de4fd1822613395131099e15aea361c406b
Sagadat Zhumakhan <sagadat.zhumakhan@gmail.com>
2024-11-27 14:54:13 +0100

    Added Mapbox

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,5 +1,17 @@
 class ToiletsController < ApplicationController
   def index
+    @your_models = YourModel.all
+
+    @markers = @your_models.map do |model|
+      {
+        lat: model.latitude,
+        lng: model.longitude,
+        info_window: render_to_string(
+          partial: "info_window",
+          locals: { model: model }
+        )
+      }
+    end
   end
 
   def show
