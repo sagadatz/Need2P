67d2b204f5d16c15def80b42b5e9ca55f02eebcd
Sagadat Zhumakhan <sagadat.zhumakhan@gmail.com>
2024-12-02 10:44:31 +0100

    Added average rating to toilets

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -6,4 +6,9 @@ class Toilet < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  def average_rating
+    return 0 if reviews.empty?
+    reviews.average(:rating).to_f.round(1)
+  end
 end
